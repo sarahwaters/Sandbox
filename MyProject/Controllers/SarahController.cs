08cddb7d02a5fc658a9c2d4ac78c0224bb3a28d9
Sarah Waters <sarah.waters@esendex.com>
2016-06-08 15:23:18 +0100

    Initial commit

---
+++ MyProject/Controllers/SarahController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyProject.Controllers
+{
+    public class SarahController : Controller
+    {
+        // GET: Sarah
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
